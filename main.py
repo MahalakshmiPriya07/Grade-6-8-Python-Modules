19d2c200330ff8680288b03ad04b9835a13175b6
MahalakshmiPriya07 <mahalakshmipriya0707@gmail.com>
2024-09-14 16:16:32 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+print("Hello World")
+
+print("Welcome to Codingal!")
+
+print("Hi I am Priya")
+
+print("This is Python Print Statement")
+
+print("Here we can display or print output/text using this statement")
+
+print("The result of 5+5 is ",5+5)
