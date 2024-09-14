688a061278d544ab23da6c678758353e59de5c1b
MahalakshmiPriya07 <mahalakshmipriya0707@gmail.com>
2024-09-14 22:59:24 +0530

    add the file

---
+++ ANDOR.py
@@ -0,0 +1,9 @@
+a = 34
+b = 45
+c = 23
+
+if a > b or a < c:
+    print("I am using or operator")
+elif a < b and a > c:
+    print("I am using and operator")
+
---
+++ Greaterno.py
@@ -0,0 +1,6 @@
+no1=int(input("Enter first number:"))
+no2=int(input("Enter second number:"))
+if( no1>no2):
+  print(no1,"is number greater")
+else:
+  print(no2,"is number greater")
