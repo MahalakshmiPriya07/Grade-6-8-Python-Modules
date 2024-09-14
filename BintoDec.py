06c52ed33b9a2eb32661dbb286f553c5f3d218b3
MahalakshmiPriya07 <mahalakshmipriya0707@gmail.com>
2024-09-15 00:04:59 +0530

    addd

---
+++ BintoDec.py
@@ -0,0 +1,8 @@
+# Input from the user
+binary_str = input("Enter a binary number: ")
+
+# Convert binary to decimal
+decimal_number = int(binary_str, 2)
+
+# Output the decimal number
+print("Decimal number:", decimal_number)
